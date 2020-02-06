b8b246c64f7d7a3585d485067bcc8b61e4e516a0
zk8080 <zhangkang8080@163.com>
2020-02-06 11:37:24 +0800

    完成播放列表展示

--- src/application/Player/playList/index.js
+++ src/application/Player/playList/index.js
@@ -1,29 +1,44 @@
 /*
  * @Author: your name
  * @Date: 2020-02-03 11:07:40
- * @LastEditTime : 2020-02-06 10:41:32
+ * @LastEditTime : 2020-02-06 11:29:56
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /cloud-music/src/application/Player/playList/index.js
  */
-import React, { memo } from 'react';
-import { PlayListWrapper, ScrollWrapper } from './style';
+import React, { memo, useRef, useState, useCallback } from 'react';
+import { PlayListWrapper, ScrollWrapper, ListContent, ListHeader } from './style';
 import { connect } from 'react-redux';
-import { changeShowPlayList } from '../store/actionCreators';
+import { changeShowPlayList, changeCurrentIndex, changePlayMode, changePlayList } from '../store/actionCreators';
 import { CSSTransition } from 'react-transition-group';
-import { useRef } from 'react';
-import { useState } from 'react';
-import { useCallback } from 'react';
+import Scroll from '../../../baseUI/scroll';
+import { playMode } from "../../../api/config";
+import { getName, shuffle, findIndex } from './../../../api/utils';
 
 function PlayList(props) {
-    const { showPlayList } = props;
-    console.log( showPlayList, '--showPlayList--' )
-    
-    const { togglePlayListDispatch } = props;
+    const {
+        showPlayList,
+        currentIndex,
+        playList: immutablePlayList,
+        currentSong: immutableCurrentSong,
+        mode,
+        sequencePlayList: immutableSequencePlayList
+    } = props;
+
+    const {
+        togglePlayListDispatch,
+        changeCurrentIndexDispatch,
+        changeModeDispatch,
+        changePlayListDispatch,
+    } = props;
+
+    const playList = immutablePlayList.toJS();
+    const currentSong = immutableCurrentSong.toJS();
+    const sequencePlayList = immutableSequencePlayList.toJS();
 
     const playListRef = useRef();
     const listWrapperRef = useRef();
-    const [ isShow, setIsShow ] = useState(false);
+    const [isShow, setIsShow] = useState(false);
 
     const onEnterCB = useCallback(() => {
         // 让列表显示
@@ -45,43 +60,134 @@ function PlayList(props) {
     }, []);
 
     const onExitedCB = useCallback(() => {
-        setIsShow (false);
+        setIsShow(false);
         listWrapperRef.current.style["transform"] = `translate3d(0px, 100%, 0px)`;
     }, [])
+
+    const getCurrentIcon = (item) => {
+        // 是不是当前正在播放的歌曲
+        const current = currentSong.id === item.id;
+        const className = current ? 'icon-play' : '';
+        const content = current ? '&#xe6e3;' : '';
+        return (
+            <i className={`current iconfont ${className}`} dangerouslySetInnerHTML={{ __html: content }}></i>
+        )
+    };
+    const getPlayMode = () => {
+        let content, text;
+        if (mode === playMode.sequence) {
+            content = "&#xe625;";
+            text = "顺序播放";
+        } else if (mode === playMode.loop) {
+            content = "&#xe653;";
+            text = "单曲循环";
+        } else {
+            content = "&#xe61b;";
+            text = "随机播放";
+        }
+        return (
+            <div>
+                <i className="iconfont" onClick={(e) => changeMode(e)} dangerouslySetInnerHTML={{ __html: content }}></i>
+                <span className="text" onClick={(e) => changeMode(e)}>{text}</span>
+            </div>
+        )
+    };
+    const changeMode = (e) => {
+        let newMode = (mode + 1) % 3;
+        // 具体逻辑比较复杂 后面来实现
+    };
 
     return (
         <CSSTransition
             in={showPlayList}
-            timeout={300} 
+            timeout={300}
             classNames="list-fade"
             onEnter={onEnterCB}
             onEntering={onEnteringCB}
             onExiting={onExitingCB}
             onExited={onExitedCB}
         >
-            <PlayListWrapper 
-                ref={playListRef} 
+            <PlayListWrapper
+                ref={playListRef}
                 style={isShow === true ? { display: "block" } : { display: "none" }}
                 onClick={() => togglePlayListDispatch(false)}
             >
                 <div className="list_wrapper" ref={listWrapperRef}>
-                    <ScrollWrapper></ScrollWrapper>
+                    <ListHeader>
+                        <h1 className="title">
+                            {getPlayMode()}
+                            <span className="iconfont clear">&#xe63d;</span>
+                        </h1>
+                    </ListHeader>
+                    <ScrollWrapper>
+                        <Scroll
+                        // ref={listContentRef}
+                        // onScroll={pos => handleScroll(pos)}
+                        // bounceTop={false}
+                        >
+                            <ListContent>
+                                {
+                                    playList.map((item, index) => {
+                                        return (
+                                            <li className="item" key={item.id}>
+                                                {getCurrentIcon(item)}
+                                                <span className="text">{item.name} - {getName(item.ar)}</span>
+                                                <span className="like">
+                                                    <i className="iconfont">&#xe601;</i>
+                                                </span>
+                                                <span className="delete">
+                                                    <i className="iconfont">&#xe63d;</i>
+                                                </span>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ListContent>
+                        </Scroll>
+                    </ScrollWrapper>
+                    {/* <Confirm
+                        ref={confirmRef}
+                        text={"是否删除全部?"}
+                        cancelBtnText={"取消"}
+                        confirmBtnText={"确定"}
+                        handleConfirm={handleConfirmClear}
+                    /> */}
                 </div>
             </PlayListWrapper>
         </CSSTransition>
-        
+
     )
 }
 
 const mapStateToProps = (state) => ({
+    currentIndex: state.getIn(['player', 'currentIndex']),
+    currentSong: state.getIn(['player', 'currentSong']),
+    playList: state.getIn(['player', 'playList']),//播放列表
+    sequencePlayList: state.getIn(['player', 'sequencePlayList']),//顺序排列时的播放列表
     showPlayList: state.getIn(['player', 'showPlayList']),
+    mode: state.getIn(['player', 'mode'])
 });
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        togglePlayListDispatch (data) {
+        togglePlayListDispatch(data) {
             dispatch(changeShowPlayList(data));
-        }
+        },
+        //修改当前歌曲在列表中的index，也就是切歌
+        changeCurrentIndexDispatch(data) {
+            dispatch(changeCurrentIndex(data));
+        },
+        //修改当前的播放模式
+        changeModeDispatch(data) {
+            dispatch(changePlayMode(data));
+        },
+        //修改当前的歌曲列表
+        changePlayListDispatch(data) {
+            dispatch(changePlayList(data));
+        },
+        // deleteSongDispatch(data) {
+        //     dispatch(deleteSong(data));
+        // },
     }
 }
 
